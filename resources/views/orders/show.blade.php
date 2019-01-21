43bbbbe895985d78a207af47795963b53428820a
marsccyu <marsccyu@gmail.com>
2019-01-21 15:55:31 +0800

    8.4 1. 收貨功能按鈕

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -45,6 +45,18 @@
                             <div class="line"><div class="line-label">收貨地址：</div><div class="line-value">{{ join(' ', $order->address) }}</div></div>
                             <div class="line"><div class="line-label">訂單備註：</div><div class="line-value">{{ $order->remark ?: '-' }}</div></div>
                             <div class="line"><div class="line-label">訂單編號：</div><div class="line-value">{{ $order->no }}</div></div>
+                            <!-- 输出物流状态 -->
+                            <div class="line">
+                                <div class="line-label">物流狀態：</div>
+                                <div class="line-value">{{ \App\Models\Order::$shipStatusMap[$order->ship_status] }}</div>
+                            </div>
+                            <!-- 如果有物流信息则展示 -->
+                            @if($order->ship_data)
+                                <div class="line">
+                                    <div class="line-label">物流資訊：</div>
+                                    <div class="line-value">{{ $order->ship_data['express_company'] }} {{ $order->ship_data['express_no'] }}</div>
+                                </div>
+                            @endif
                         </div>
                         <div class="order-summary text-right">
                             <div class="total-amount">
@@ -53,6 +65,7 @@
                             </div>
                             <div>
                                 <span>訂單狀態：</span>
+
                                 <div class="value">
                                     @if($order->paid_at)
                                         @if($order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
@@ -66,11 +79,67 @@
                                         未支付
                                     @endif
                                 </div>
+                                @if(isset($order->extra['refund_disagree_reason']))
+                                    <div>
+                                        <span>拒絕請款理由：</span>
+                                        <div class="value">{{ $order->extra['refund_disagree_reason'] }}</div>
+                                    </div>
+                                @endif
                             </div>
+                            <!-- 支付按钮开始 -->
+                            @if(!$order->paid_at && !$order->closed)
+                                <div class="payment-buttons">
+                                    <a class="btn btn-primary btn-sm" href="{{ route('payment.alipay', ['order' => $order->id]) }}">支付寶支付</a>
+                                    <!-- 把之前的微信支付按钮换成这个 -->
+                                    <button class="btn btn-sm btn-success" id='btn-wechat'>微信支付</button>
+                                </div>
+                            @endif
+                            <!-- 如果订单的发货状态为已发货则展示確認收貨按钮 -->
+                            @if($order->ship_status === \App\Models\Order::SHIP_STATUS_DELIVERED)
+                                <div class="receive-button">
+                                    <button type="button" id="btn-receive" class="btn btn-sm btn-success">確認收貨</button>
+                                </div>
+                            @endif
+                            <!-- 订单已支付，且退款状态是未退款时展示申請退款按钮 -->
+                            {{--@if($order->paid_at && $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)--}}
+                                {{--<div class="refund-button">--}}
+                                    {{--<button class="btn btn-sm btn-danger" id="btn-apply-refund"><申請退款></申請退款></button>--}}
+                                {{--</div>--}}
+                            {{--@endif--}}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+@section('scriptsAfterJs')
+    <script>
+        $(document).ready(function() {
+            // 确认收货按钮点击事件
+            $('#btn-receive').click(function() {
+                // 弹出确认框
+                swal({
+                    title: "確認已經收到商品？",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                    buttons: ['取消', '確認收到'],
+                })
+                    .then(function(ret) {
+                        // 如果点击取消按钮则不做任何操作
+                        if (!ret) {
+                            return;
+                        }
+                        // ajax 提交确认操作
+                        axios.post('{{ route('orders.received', [$order->id]) }}')
+                            .then(function () {
+                                // 刷新页面
+                                location.reload();
+                            })
+                    });
+            });
+
+        });
+    </script>
 @endsection
